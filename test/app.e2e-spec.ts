b38cd73aa757ef68f3d7982ef70e0cbe33b41612
RaeKyoNeko <marchjeff@gmail.com>
2023-04-25 02:16:23 +0700

    create template logic for bookmark route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from '@src/auth/dto';
+import { EditUserDto } from '@src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -123,7 +124,24 @@ describe('App e2e', () => {
       });
     });
 
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+      it('Should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Rayujin',
+          email: 'rayujin@codewithrayujin.com',
+        };
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.email)
+          .expectBodyContains(dto.firstName);
+      });
+    });
   });
 
   describe('Bookmark', () => {
